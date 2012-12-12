1746f3de4afee901f23a739f88d23806d04ddf2e
abmargb <abmargb@gmail.com>
2012-12-12 00:41:56 -0300

    Added RSM scrolling to affiliations retrieval.

--- src/com/buddycloud/pusher/message/ConsumerUtils.java
+++ src/com/buddycloud/pusher/message/ConsumerUtils.java
@@ -12,39 +12,57 @@ import com.buddycloud.pusher.XMPPComponent;
 public class ConsumerUtils {
 
 	public static final String PUBSUB_NS = "http://jabber.org/protocol/pubsub";
+	public static final String RSM_NS = "http://jabber.org/protocol/rsm";
 	public static final String PUBSUB_OWNER_NS = PUBSUB_NS + "#owner";
 	public static final String PUBSUB_EVENT_NS = PUBSUB_NS + "#event";
-	
-//	<iq from="channeluser@example.com/ChannelCompatibleClient" to="channelserver.example.com" type="get" id="26:85"> 
-//	  <pubsub xmlns="http://jabber.org/protocol/pubsub#owner"> 
-//	    <affiliations node="/user/channeluser@example.com/posts"/> 
-//	    <set xmlns="http://jabber.org/protocol/rsm"> 
-//	      <max>30</max> 
-//	    </set> 
-//	  </pubsub> 
-//	</iq>
 	
 	@SuppressWarnings("unchecked")
 	public static List<Affiliation> getAffiliations(String node, XMPPComponent xmppComponent) throws XMPPException {
 		String channelServerJid = xmppComponent.getProperty("channel.server");
+		IQ affiliationRequestIq = createAffiliationsRequest(node, channelServerJid, null);
+		
+		List<Affiliation> affiliations = new LinkedList<Affiliation>();
+		while (true) {
+			IQ affiliationsIq = xmppComponent.syncIQ(affiliationRequestIq);
+			
+			Element pubsubResponseEl = affiliationsIq.getChildElement();
+			Element affiliationsResponseEl = pubsubResponseEl.element("affiliations");
+			List<Element> affiliationsChildren = affiliationsResponseEl.elements("affiliation");
+			
+			for (Element affiliationEl : affiliationsChildren) {
+				affiliations.add(Affiliation.parse(affiliationEl));
+			}
+			
+			Element rsmEl = pubsubResponseEl.element("rsm");
+			if (rsmEl == null) {
+				break;
+			}
+			Element lastEl = rsmEl.element("last");
+			if (lastEl == null) {
+				break;
+			}
+			
+			affiliationRequestIq = createAffiliationsRequest(node, channelServerJid, 
+					lastEl.getText());
+		}
+		
+		return affiliations;
+	}
+
+	private static IQ createAffiliationsRequest(String node,
+			String channelServerJid, String after) {
 		IQ affiliationRequestIq = new IQ();
 		affiliationRequestIq.setTo(channelServerJid);
 		Element pubsubEl = affiliationRequestIq.getElement().addElement("pubsub", PUBSUB_OWNER_NS);
 		Element affiliationsEl = pubsubEl.addElement("affiliations");
 		affiliationsEl.addAttribute("node", node);
 		
-		IQ affiliationsIq = xmppComponent.syncIQ(affiliationRequestIq);
-		
-		List<Affiliation> affiliations = new LinkedList<Affiliation>();
-		Element affiliationsResponseEl = affiliationsIq.getChildElement().element("affiliations");
-		List<Element> affiliationsChildren = affiliationsResponseEl.elements("affiliation");
-		
-		//TODO RSM
-		for (Element affiliationEl : affiliationsChildren) {
-			affiliations.add(Affiliation.parse(affiliationEl));
+		if (after != null) {
+			Element rsmEl = pubsubEl.addElement("set", RSM_NS);
+			rsmEl.addElement("after").setText(after);
 		}
 		
-		return affiliations;
+		return affiliationRequestIq;
 	}
 	
 	public static List<Affiliation> getOwners(String node, XMPPComponent xmppComponent) throws XMPPException {
@@ -66,5 +84,9 @@ public class ConsumerUtils {
 		itemsEl.addAttribute("node", node);
 		itemsEl.addElement("item").addAttribute("id", replyRef);
 		return xmppComponent.syncIQ(iq);
+	}
+	
+	public static String getChannelAddress(String nodeJid) {
+		return nodeJid.split("/")[2];
 	}
 }
